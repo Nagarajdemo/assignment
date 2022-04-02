be2354706ee5c0ea4f29440e126e5c8263b9e8df
unknown <nagaraja1978@gmail.com>
2022-04-03 01:51:17 +0530

    renamed module

--- api_automation/src/main/java/com/qa/driver/Base.java
+++ api_automation/src/main/java/com/qa/driver/Base.java
@@ -1,4 +1,13 @@
 package com.qa.driver;
 
+import org.openqa.selenium.WebDriver;
+
 public class Base {
+    public WebDriver driver;
+    public static ThreadLocal<WebDriver> webDriverThreadLocal=new ThreadLocal<>();
+    public void initialiseDriver(String browser){
+        if(browser.equalsIgnoreCase("chrome")){
+
+        }
+    }
 }
