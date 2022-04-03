b10a45f6bb6877310117162564d24cfd7c4594da
unknown <nagaraja1978@gmail.com>
2022-04-03 22:20:38 +0530

    modified the step file

--- ui_automation_assignment/src/test/java/stepdefinitions/Search.java
+++ ui_automation_assignment/src/test/java/stepdefinitions/Search.java
@@ -11,21 +11,20 @@ public class Search {
 
     private GooglePage googlePage=new GooglePage(BaseInit.getDriver());
     @Given("I have to search companyName via google")
-    public void i_have_to_search_company_name_via_google() {
+    public void navigateToGoogle() {
         BaseInit.getDriver().get("http://www.google.com");
         Assert.assertEquals("Sign in",googlePage.checkForGoogle());
         System.out.println(googlePage.checkForGoogle());
     }
     @When("I enter the {string} in the search field")
-    public void i_enter_the_in_the_search_field(String companyName) {
+    public void SearchByCompanyName(String companyName) {
         googlePage.searchBy(companyName);
         System.out.println("when "+companyName);
         // Write code here that turns the phrase above into concrete actions
 
     }
     @Then("validate the company logo")
-    public void validate_the_company_logo() {
-        System.out.println("then ");
+    public void validateCompanyName() {
         Assert.assertEquals("J.P. MORGAN",googlePage.returnCompanyLogo());
 
         // Write code here that turns the phrase above into concrete actions
