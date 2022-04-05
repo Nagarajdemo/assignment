08a95fd87227afc04c613b0c080f975abf5c0cc6
unknown <nagaraja1978@gmail.com>
2022-04-05 23:57:34 +0530

    added runner file

---
+++ api_automation/src/test/java/stepdefinitions/TestRunner/TestRunner.java
@@ -0,0 +1,19 @@
+package stepdefinitions.TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features={"src/test/resources/stories"},
+    glue={"stepdefinitions"},
+    plugin={"pretty"}
+
+        )
+public class TestRunner {
+
+}
